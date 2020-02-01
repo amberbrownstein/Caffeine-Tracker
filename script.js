437b4c6a2f439a552516ebf30e1726ca7c02244c
Amber Brownstein <amberbrownstein@gmail.com>
2020-01-31 21:33:44 -0500

    update script.js
    wrote the basic equation for the calculation algorithm

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-function calc(){
-	
+Using Math.pow
+
+function calc(weight, items, now){
+	var blood = .07 * weight;
+	blood = blood / 2.34;
+	var mgCaf = 0;
+
+	for (const i of items) {
+		t = now - i.time
+		mgCaf = mgCaf + i.mgCaf * Math.pow(2, -4 * t)
+	}
+
+	var mgL = mgCaf / blood
 }
