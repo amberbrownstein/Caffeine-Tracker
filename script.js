39ad3e4e7e72a8d787055d6f75df69f96ceb6cb3
Amber Brownstein <amberbrownstein@gmail.com>
2020-02-02 03:00:53 -0500

    done

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ class Item {
 
 		switch(name) {
 			case "Soda":
-				this.caf = 40
+				this.caf = 45
 				break;
 			case "Green Tea":
 				this.caf = 28
@@ -18,16 +18,16 @@ class Item {
 				this.caf = 47
 				break;
 			case "Energy Drink":
-				this.caf = 200
+				this.caf = 158
 				break;
 			case "Dark Chocolate":
 				this.caf = 23
 				break;
 			case "Regular Coffee":
-				this.caf = 96
+				this.caf = 95
 				break;
 			case "Espresso":
-				this.caf = 40
+				this.caf = 64
 				break;
 			case "Custom":
 				this.caf = amt;
@@ -67,14 +67,13 @@ function addRow(){
 
 	var del = document.createElement("INPUT");
 	del.setAttribute("type", "button");
-	del.innerHTML = "delete";
+	del.value = "delete";
 	row.appendChild(del);
 	del.addEventListener("click", function() { delItem(row); });
 }
 
 function delItem(row){
-	var i = row.parentNode.parentNode.rowIndex;
- 	document.getElementById("items").deleteRow(i);
+ 	document.getElementById("items").removeChild(row);
 }
 
 function calc(){
@@ -87,21 +86,48 @@ function calc(){
 		items.push(item);
 	}
 
-	var mgCaf = equation(document.getElementById("weight").value, items, Date.now());
+	var blood = .07 * document.getElementById("weight").value / 2.34;
+	var mgCaf = equation(blood, items);
 	var out = document.getElementById("out");
 	out.innerHTML = "Your current caffeine level is approximately " + mgCaf + "mg/L.";
+	var end = filter(mgCaf, blood).toFixed(2);
+	if(mgCaf >= 80){
+		var txt = document.createTextNode('You have consumed lethal amounts of caffeine. It will take ' + end + ' hours to return to normal levels.');
+		out.appendChild(txt);
+	}
+	else if(mgCaf > 15){
+		var txt = document.createTextNode('You are probably starting to feel the negative side effects of caffeine. It will probably take around ' + end + ' hours before these effects start to diminish.');
+		out.appendChild(txt);
+	}
+	else{
+		var txt = document.createTextNode('You can consume ' + end + ' more mg of caffeine before you may start to feel adverse side effects.');
+		out.appendChild(txt);
+	}
 }
 
-function equation(weight, items, now){
-	var blood = .07 * weight;
-	blood = blood / 2.34;
+function equation(blood, items){
+	var now = new Date();
 	var mgCaf = 0;
 
+	var dd = String(now.getDate()).padStart(2, '0');
+	var mm = String(now.getMonth() + 1).padStart(2, '0');
+	var yyyy = now.getFullYear();
+
+	var today = mm + '/' + dd + '/' + yyyy;
+
 	for (const i of items) {
-		var time = i.time;
-		var t = now - Date.parse(time);
-		mgCaf = mgCaf + parseFloat(i.mgCaf) * Math.pow(2, -4 * t);
+		var t = Date.parse(today + ' ' + i.time);
+		t = now - t;
+		t = t / (1000 * 60 * 60);
+		t = t / 5.7;
+		mgCaf = mgCaf + parseFloat(i.mgCaf) * Math.pow(2, 0 - t);
 	}
 
-	return mgCaf / blood;
+	return (mgCaf / blood).toFixed(2);
+}
+
+function filter(start, blood){
+	if(start > 15)
+		return 5.7 * Math.log2(start/15)
+	else return (15 - start) * blood
 }
